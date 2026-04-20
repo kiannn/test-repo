36fdc104dd44c60cae1f4dd973e9dd3dcc6f7ccc
kiannn <kian1986@live.com>
2026-04-20 19:08:42 -0400

    changes commited in cloned copy

--- src/main/java/repTEST/demo/DemoApplication.java
+++ src/main/java/repTEST/demo/DemoApplication.java
@@ -8,6 +8,9 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+                //=========Thr following changes are made in cloned version===
+                short s =2;
+                System.out.println("repTEST.demo.DemoApplication.main()");
 	}
 
 }
